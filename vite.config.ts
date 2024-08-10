970cf4e9544c959b166e4e1f209b5be740070986
wateen0112 <wateenoaswad2000@gmail.com>
2024-08-10 20:05:15 +0300

    dd

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
   plugins: [vue()],
+  // base: './',         // works, but can clash with createWebHistory
+  // base: '/',          // if the app is in root directory
+  base: '/',  // if the app is in sub path
+  build: {
+    watch:true ,
+    chunkSizeWarningLimit: 5000,
+  },
+  optimizeDeps: {
+    exclude: ['vuetify'],
+    entries: [
+      './src/**/*.vue',
+    ],
+  },
 })
